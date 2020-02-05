7c2f2cba3ff5cc88b3e517dd4430ec1cf9973d4e
PatxiofromAlphensign <aissypewdiepie@gmail.com>
2020-02-05 19:16:31 +0530

    test

--- rng.py
+++ rng.py
@@ -2,9 +2,14 @@ import os, random
 
 lil = []
 i = 0
-while i < 10:
+n = int(input("how much?"))/2
+while i < n:
     i += 1
-    lil.append(random.randint(0,100))
+    lil.append(random.randint(10,100))
 
-number = "".join(str(x) for x in lil)
-print(number)
+#print(int(n))
+#for x in lil:
+#while True:
+joined = "".join(str(x) for x in lil[:int(n)*2])
+print("+91" + joined)
+
---
+++ yourmomgay.py
@@ -0,0 +1,19 @@
+import random as rd
+import time
+import re
+def looper(n, item):
+    i = 0
+    #$ii = 0
+    lil = []
+    #while ii < 10:
+    for x in range(20):
+        while i < int(n):
+            i += 1
+            lil.append("%s" % item) 
+
+        string = str(lil)
+        print(string)
+
+x = y = input("eveb amounts, then how many?")
+while True:
+    looper(x,"your mom gay")
