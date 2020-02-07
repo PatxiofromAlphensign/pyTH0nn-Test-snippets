602384cb45c4427f4fb4bd0fac92a67ff74b91cf
PatxiofromAlphensign <aissypewdiepie@gmail.com>
2020-02-07 11:13:13 +0530

    returns endless soup of string 'gay' (hopefully)

--- yourmomgay.py
+++ yourmomgay.py
@@ -1,5 +1,5 @@
 import random as rd
-import time
+import time 
 import re
 def looper(n, item):
     i = 0
@@ -9,11 +9,21 @@ def looper(n, item):
     for x in range(20):
         while i < int(n):
             i += 1
-            lil.append("%s" % item) 
+            lil.append("%s" % str(item)) 
 
-        string = str(lil)
-        print(string)
+            string = "".join(str(b) for b in lil)
+            return string
 
-x = y = input("eveb amounts, then how many?")
+#x = y = input("eveb amounts, then how many?")
+
+# make this so the number of items in the array keeps increasing
+v = 10 
+looper = looper(12, "gay")
 while True:
-    looper(x,"your mom gay")
+    #time.sleep(3)    
+    #v += 1
+    print(looper)
+    if str(looper) == "":
+        break
+    else:
+        continue
